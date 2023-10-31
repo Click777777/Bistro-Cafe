4cded15a9f53794caf58f5f8dd07889caee74888
Soe Wunna Htun <soewunnahtun176811@gmail.com>
2023-10-31 13:46:17 +0630

    .

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from "react";
+
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Menu from "./components/Menu";
@@ -7,6 +9,27 @@ import Reservation from "./components/Reservation";
 import Footer from "./components/Footer";
 
 const App = () => {
+  const [isLoading, setLoading] = useState(true);
+
+  function someRequest() {
+    //Simulates a request; makes a "promise" that'll run for 2.5 seconds
+    return new Promise((resolve) => setTimeout(() => resolve(), 2500));
+  }
+
+  useEffect(() => {
+    someRequest().then(() => {
+      const loaderElement = document.querySelector(".loader-container");
+      if (loaderElement) {
+        loaderElement.remove();
+        setLoading(!isLoading);
+      }
+    });
+  });
+
+  if (isLoading) {
+    return null;
+  }
+
   return (
     <div className="mx-auto h-full max-w-[1800px] overflow-hidden bg-pattern bg-no-repeat ">
       <Hero />
